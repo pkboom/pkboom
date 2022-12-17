f0f934bdd1df193b371ed67d29b68565251c20c0
Keunbae Park <pkboom@users.noreply.github.com>
2022-12-17 07:18:59 -0500

    wip

--- build-svg.js
+++ build-svg.js
@@ -2,6 +2,8 @@ const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 
 let fs = require('fs')
 let got = require('got')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 
 const today = new Date()
 const todayDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(today)
@@ -28,6 +30,24 @@ let url = `data/2.5/weather?q=Ancaster,ON,Canada&APPID=${WEATHER_API_KEY}&units=
 
     let icon = 'data:' + response.headers['content-type'] + ';base64,' + Buffer.from(response.body).toString('base64')
 
+    const { body: filmBody } = await got('https://top10.netflix.com/films')
+
+    let dom = new JSDOM(filmBody, {
+      url: 'https://top10.netflix.com/films',
+      contentType: 'text/html',
+    })
+
+    const topFilm = `Netflix top film is "${dom.window.document.querySelector('.list-table tbody tr td:nth-child(2)').textContent}"`
+
+    const { body: tvBody } = await got('https://top10.netflix.com/tv')
+
+    dom = new JSDOM(tvBody, {
+      url: 'https://top10.netflix.com/tv',
+      contentType: 'text/html',
+    })
+
+    const topTv = `and top tv show is "${dom.window.document.querySelector('.list-table tbody tr td:nth-child(2)').textContent}".`
+
     fs.readFile('template.svg', 'utf-8', (error, data) => {
       if (error) {
         return
@@ -39,6 +59,9 @@ let url = `data/2.5/weather?q=Ancaster,ON,Canada&APPID=${WEATHER_API_KEY}&units=
       data = data.replace('{description}', description)
       data = data.replace('{icon}', icon)
       data = data.replace('{todayDay}', todayDay)
+      data = data.replace('{topFilm}', topFilm)
+      data = data.replace('{topTv}', topTv)
+      data = data.replace('{netflixLength}', Math.max(topFilm.length, topTv.length) * 9)
       data = fs.writeFile('chat.svg', data, (err) => {
         if (err) {
           console.error(err)
